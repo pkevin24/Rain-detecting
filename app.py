a9c39ae43d55b374f945bcf791287b9517b9838a
pkevin24 <70275725+pkevin24@users.noreply.github.com>
2021-05-18 23:50:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+import pandas as pd 
+
+# d1=pd.read_csv("adjjj-c8739-default-rtdb-export (2).csv")
+# d1=np.array(d1)
+# X=d1[0][0]
+# y=d1[0][1]
+
+gsheetid = "1lA027q2n8QCEH5CERkcztzyG7mSWHqMkTx4CBLK5TGY"
+sheet_name = "Sheet1"
+gsheet_url = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid, sheet_name)
+df = pd.read_csv(gsheet_url,index_col=False)
+
+gsheetid1 = "18spSY7iiWZuZcFH8G9GqvQ5D--rzKaMASAGUGJh10bY"
+sheet_name1 = "Sheet1"
+gsheet_url1 = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid1, sheet_name1)
+df1 = pd.read_csv(gsheet_url1, index_col=False)
+
+d1=pd.merge(df1, df, left_index=True, right_index=True)
+a=d1.tail(1)
+a=np.array(a)
+X=a[0][0]
+y=a[0][1]
+
+
+
+model=pickle.load(open('logisticrefrain.pkl','rb'))
+
+app=Flask(__name__,template_folder='template')
+
+
+@app.route('/')
+def man():
+    return render_template("rain.html")
+
+
+@app.route('/predict',methods=['POST'])
+def home():
+    a=request.form['Temperature']
+    b=request.form['Humidity']
+    arr=np.array([[a,b]])
+    pred=model.predict(arr)
+
+    # output='{0:.{1}f}'.format(pred[0][1], 2)
+    output=pred
+    return render_template('rainafter.html',data=output)
+
+@app.route('/predict1',methods=['POST'])
+def fb():
+    arr1=np.array([[X,y]])
+    # arr1=arr1.reshape(-1,1)
+    pred=model.predict(arr1)
+
+    # output='{0:.{1}f}'.format(pred[0][1], 2)
+    output=pred
+    return render_template('rainafter.html',data=output)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ raindet.py
@@ -0,0 +1,55 @@
+#Importing all the reqired libraries
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from statistics import mean 
+
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+import pickle
+data=pd.read_csv("/static/temphumdetails.csv")
+data=pd.DataFrame(data)
+def fahr_to_celsius(temp_fahr):
+    """Convert Fahrenheit to Celsius
+    
+    Return Celsius conversion of input"""
+    temp_celsius = (temp_fahr - 32) * 5 / 9
+    return temp_celsius
+
+data["tempC"] = fahr_to_celsius(data["TempAvgF"])
+data['Events'].replace(' ',np.nan, inplace=True)
+data["Events"].replace([np.nan,"Fog"],0,inplace=True)
+data["Events"].replace(["Rain , Thunderstorm","Rain","Rain , Snow","Fog , Rain , Thunderstorm","Thunderstorm","Fog , Thunderstorm","Fog , Rain"],1,inplace=True)
+
+data1=pd.DataFrame(data[['tempC',"HumidityAvgPercent","Events"]])
+
+data1['HumidityAvgPercent']=data1['HumidityAvgPercent'].astype(str)
+
+def convert(data):
+    number = preprocessing.LabelEncoder()
+    data['HumidityAvgPercent'] = number.fit_transform(data['HumidityAvgPercent'])
+    data=data.fillna(-999) # fill holes with default value
+    return data
+
+
+data1=convert(data1)
+
+data1=np.array(data1)
+
+X = data1[0:, 0:2]
+y = data1[0:, -1]
+
+y = y.astype('int')
+X = X.astype('float')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+lr=LogisticRegression()
+lr.fit(X_train,y_train)
+
+file=open('logisticrefrain.pkl','wb')
+pickle.dump(lr,file)
